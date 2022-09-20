5d8785bf7e85ea0b418dc6e3319f43aff9804543
Gustavo Lima <gustavolimaeduardo@gmail.com>
2022-09-20 13:01:31 -0300

    validação para enviar

--- dist/api/services/EmailService.js
+++ dist/api/services/EmailService.js
@@ -16,8 +16,55 @@ const EmailTemplateService_1 = __importDefault(require("../../helpers/email/Emai
 class UsuarioService {
     send(data) {
         return __awaiter(this, void 0, void 0, function* () {
-            let retorno = EmailTemplateService_1.default.novaPesquisa(data);
+            let preenchido = yield this.validar(data);
+            if (preenchido) {
+                var retorno = EmailTemplateService_1.default.novaPesquisa(data);
+            }
+            else {
+                var retorno = "Campos ausentes";
+            }
             return retorno;
+        });
+    }
+    validar(data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!data.valorProduto || data.valorProduto == '' || data.valorProduto == "")
+                return false;
+            if (!data.notobookEmMaos || data.notobookEmMaos == '' || data.notobookEmMaos == "")
+                return false;
+            if (!data.modelo || data.modelo == '' || data.modelo == "")
+                return false;
+            if (!data.anoFabricacao || data.anoFabricacao == '' || data.anoFabricacao == "")
+                return false;
+            if (!data.dataCompra || data.dataCompra == '' || data.dataCompra == "")
+                return false;
+            if (!data.nome || data.nome == '' || data.nome == "")
+                return false;
+            if (!data.dataNascimento || data.dataNascimento == '' || data.dataNascimento == "")
+                return false;
+            if (!data.rg || data.rg == '' || data.rg == "")
+                return false;
+            if (!data.email || data.email == '' || data.email == "")
+                return false;
+            if (!data.celular || data.celular == '' || data.celular == "")
+                return false;
+            if (!data.cep || data.cep == '' || data.cep == "")
+                return false;
+            if (!data.endereco || data.endereco == '' || data.endereco == "")
+                return false;
+            if (!data.numeroCasa || data.numeroCasa == '' || data.numeroCasa == "")
+                return false;
+            if (!data.complemento || data.complemento == '' || data.complemento == "")
+                return false;
+            if (!data.cidade || data.cidade == '' || data.cidade == "")
+                return false;
+            if (!data.uf || data.uf == '' || data.uf == "")
+                return false;
+            if (!data.cpf || data.cpf == '' || data.cpf == "")
+                return false;
+            if (!data.termos || data.termos == '' || data.termos == "")
+                return false;
+            return true;
         });
     }
 }
